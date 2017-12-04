3a4a48ea532374ed16e4e3e0effccb47ae8a5568
Nikhitha1234 <Nikhitha.ksr71@gmail.com>
2017-12-03 21:56:03 -0800

    Minor Changes

--- db_conn.py
+++ db_conn.py
@@ -4,12 +4,19 @@ import MySQLdb
 import uuid
 
 
-def generateQuery(checkin, checkout, rooms):
+def generateQuery(response):
     db = MySQLdb.connect("localhost","root","passw0rd","Hotels" )
+    for i in response:
+    	hotel_name = response[hotel_name]
+    	rooms=response[rooms]
+    	checkin = response[checkin_year]+"/"+response[checkin_month]+"/"+response[checkin_day]
+    	checkout = response[checkout_year]+"/"+response[checkout_month]+"/"+response[checkout_day]
+    	amenities=response[amenities]
+    	room_type=response[room_type]
     cursor = db.cursor()
     booking_id = "booking" + str(uuid.UUID.hex)
-    cursor.execute(("Insert into reservation values (" + booking_id + "," +
-                    "," + checkin + "," + checkout + "," + str(rooms) + ")" ))
+    cursor.execute(("Insert into reservation values ("  + booking_id + "," +hotel_name+ "," +
+                    "," + checkin + "," + checkout + "," + str(rooms) + "," +amenities+ "," + room_type ")" ))
 
 
 # if __name__ == "__main__":
