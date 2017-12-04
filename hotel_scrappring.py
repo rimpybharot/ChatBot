aae81c521f6081bab096f0c70d570594954cb88d
Amreen Abdul Wahab <amreenabdulwahab@gmail.com>
2017-12-03 19:43:43 -0800

    removed extra url

--- hotel_scrappring.py
+++ hotel_scrappring.py
@@ -26,7 +26,7 @@ def handle_command(command, channel):
     """
     url="https://www.booking.com/searchresults.html?label=gen173nr-1DCAEoggJCAlhYSDNYBHIFdXNfY2GIAQGYATG4AQfIAQzYAQPoAQH4AQKSAgF5qAID&sid=9cba56f10b8617d55e137c61b27b1016&age=1&age=1&age=2&checkin_month=12&checkin_monthday=6&checkin_year=2017&checkout_month=12&checkout_monthday=8&checkout_year=2017&city=-1109108&class_interval=1&dest_id=20013069&dest_type=city&dtdisc=0&fe_new_tab=0&group_adults=1&group_children=0&inac=0&index_postcard=0&label_click=undef&no_rooms=1&postcard=0&raw_dest_type=city&room1=A&sb_price_type=total&sb_travel_purpose=business&search_selected=1&src=searchresults&ss=Fremont%2C%20California%2C%20USA&ss_all=0&ss_raw=fre&ssb=empty&sshis=0&ssne_untouched=San%20Jos%C3%A9&nflt=oos%3D1%3B&lsf=oos%7C1%7C-1&unchecked_filter=out_of_stock&hide_soldouts=1"
     
-    url1 = "https://www.hotels.com/search.do?resolved-location=CITY%3A1504033%3AUNKNOWN%3AUNKNOWN&q-destination=Las%20Vegas,%20Nevada,%20United%20States%20of%20America&q-check-in=2017-12-05&q-check-out=2017-12-06&q-rooms=1&q-room-0-adults=1&q-room-0-children=0"
+    #url1 = "https://www.hotels.com/search.do?resolved-location=CITY%3A1504033%3AUNKNOWN%3AUNKNOWN&q-destination=Las%20Vegas,%20Nevada,%20United%20States%20of%20America&q-check-in=2017-12-05&q-check-out=2017-12-06&q-rooms=1&q-room-0-adults=1&q-room-0-children=0"
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
