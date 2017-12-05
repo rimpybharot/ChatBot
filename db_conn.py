8760217762a74d1313d0da8c16783308cd470540
rimpybharot <rbverses21@gmail.com>
2017-12-05 00:43:08 -0800

    Correcting insertion failures due to missing db.commit

--- db_conn.py
+++ db_conn.py
@@ -8,9 +8,11 @@ import random
 def get_reservations():
     db = MySQLdb.connect("localhost","root","passw0rd","hotel_reservation" )
     cursor = db.cursor()
-    data = cursor.execute(("select * from hotel_reservation.reservation"))
-    return data
-
+    cursor.execute("select * from `reservation`")
+    
+    bookings = cursor.fetchall()
+    db.close()
+    return bookings
 
 def generateQuery(response):
     db = MySQLdb.connect("localhost","root","passw0rd","hotel_reservation" )
@@ -34,9 +36,14 @@ def generateQuery(response):
     + "," + "\'" + str(room_type) + "\'"
     +")")
     print q
+    # q = "INSERT INTO `reservation` values ('EY-87D2C2FAKYMO','The Row Hotel','San%20Jose','2018/9/9','2018/10/9','4','Outdoor Pool, Air Conditioning/Heating, Free Wifi, Pet Friendly',' Double Room     Max people: 2  ')"
     cursor.execute(q)
+    db.commit()
+    db.close()
 
 
 
-# if __name__ == "__main__":
-#     generateQuery()
+if __name__ == "__main__":
+
+    # generateQuery("")
+    get_reservations()
